2af939370df9c7cef025471d26035d5fa8b209fe
singer2121 <singh.topperofschool@gmail.com>
2025-06-27 16:07:03 +0530

    initial commit

---
+++ lesson1/activity1.py
@@ -0,0 +1,6 @@
+print("Hello everyone") #greeting message
+print("I am Akshita Singh")
+print("I am 12 years old")
+print("My favorite color is lavender")
+print("I like playing football ")
+print("The sum of 8 and 4 is",8+4)
