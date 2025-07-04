d60bb4623ce022774ee3abf4358ffeddaf87d243
singer2121 <singh.topperofschool@gmail.com>
2025-07-04 16:04:16 +0530

    initial commit

---
+++ lesson2/activity1.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print("Welcome to the world of Coding")
+
+#Print a number
+print(12)
+
+#next line after print 
+print("Hey python \n")
+
+#Pass multiple arguments for printing
+print("I am Akshita",12,"years old")
+
+#End of arguments of print statement 
+print("Welcome to my web page", end="*")
---
+++ lesson2/activity2.py
@@ -0,0 +1,12 @@
+a = 12
+k = "star"
+print(a)
+print(k)
+
+star ="beautiful"
+print(star)
+
+#user input
+name = input("enter your name: ")
+
+print("\nHey", name, "\nNice to meet you")
