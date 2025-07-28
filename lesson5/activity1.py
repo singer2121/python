42548f0e1b0c94653f3581c86d90c890f318729e
singer2121 <singh.topperofschool@gmail.com>
2025-07-28 15:49:48 +0530

    inital commit

---
+++ lesson5/activity1.py
@@ -0,0 +1,8 @@
+a=21
+b=41
+c=8
+
+if a>b or a<c:
+    print("i am using or operator")
+elif a<b and a>c: 
+    print("i am using and oprator")
---
+++ lesson5/activity3.py
@@ -0,0 +1,12 @@
+#below statement takes input from user
+number=int(input("enter a number  "))
+
+#below statement is conditional and check for condition to be true or false
+if number>9:
+    print(number,"is a positive number")
+elif number==9:
+    print(number,"is s neutral number")
+else:
+    print("negative number")
+    a=True
+    print(not a)
