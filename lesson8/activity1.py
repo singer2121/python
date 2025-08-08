0c232330c8c4f88c19e5ec7a53b08ca2f91b78b7
singer2121 <singh.topperofschool@gmail.com>
2025-08-08 16:14:58 +0530

    inital commit

---
+++ lesson8/activity1.py
@@ -0,0 +1,10 @@
+# input the values of the terms
+n = int(input("enter the vlues of the terms:"))
+
+sum = 0 
+i = 1
+while i<=n : #loop will run from 1 to n
+    sum = sum+i
+    i = i+1
+
+print("\nsum =" , sum)
