8304f08ba809ad36e0f2709591ee879d960a5baf
singer2121 <singh.topperofschool@gmail.com>
2025-09-26 16:10:57 +0530

    inital commit

---
+++ lesson15/afterclass.py
@@ -0,0 +1,16 @@
+def check_age(age):
+    try:
+
+        age = int(age)
+        if age <0:
+             raise ValueError("age cannot be negative.")
+        if age %2 == 0:
+            print("the age {age} is even")
+        else:
+             print("the age {age} is odd")
+
+    except ValueError as e:
+        print("invalid age entered:{e}")
+
+user_input = input("enter your age :")
+check_age(user_input)
---
+++ lesson16/activity3.py
@@ -0,0 +1,21 @@
+import random
+options = ["rock","paper","scissors"]
+
+user_choice = input("choose rock, paper,or scissors:")
+computer_choice = random.choice(options)
+
+print("you chose:",user_choice)
+print("computer chose", computer_choice)
+
+if user_choice == computer_choice:
+    print("it is a tie!!")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("rock smashes scissor !! you win")
+elif user_choice == "paper" and computer_choice == "rock":
+     print("paper covetrs rock !! you win")
+
+elif user_choice == "scissors" and computer_choice == "paper":
+     print("scissors cuts paper !! you win")
+
+else:
+     print("you lose !!!")
