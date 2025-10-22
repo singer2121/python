29a34c4c793fac0c8882ec07ca9d105c8a81deb3
singer2121 <singh.topperofschool@gmail.com>
2025-10-22 15:46:05 +0530

    inital commit

---
+++ activity1.py
@@ -0,0 +1,17 @@
+tuplex = ("tuplex", False, 3,2,1)
+print(tuplex)
+
+tuplex = (4,6,8,3,1)
+print (tuplex)
+tuplex = tuplex + (9,)
+print (tuplex)
+
+tuplex = (50,10,60,70,50)
+print (tuplex.count(50))
+
+tuplex = (2,4,3,5,4,6,7,8,6,1)
+_slice = tuplex[3:5]
+print(slice)
+
+_slice = tuplex[:6]
+print(_slice)
