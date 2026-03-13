d4dc732d077f6bae1225c75c3828a95ef9612ed3
singer2121 <singh.topperofschool@gmail.com>
2026-03-13 16:14:23 +0530

    inital commit

---
+++ lesson30/activity1.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_model((400,500))
+done = False
+  while not done:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    pygame.display.flip()
