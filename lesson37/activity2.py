a5190520a8d771b136553b9442048281e8dc3b49
singer2121 <singh.topperofschool@gmail.com>
2026-04-24 16:07:32 +0530

    inital commit

---
+++ lesson37/activity2.py
@@ -0,0 +1,38 @@
+from tkinter import*
+
+root = Tk()
+root.title('Login App')
+root.geometry('400x400')
+
+frame = Frame(master=root, height=200, width=360, bg="#D878EB")
+
+lbl1 = Label(frame, text="Full Name", bg="#64A2E1", fg='black', width = 12)
+lbl2 = Label(frame, text="Email Id", bg="#64A2E1", fg='black', width = 12)
+lbl3 = Label(frame, text="Enter password", bg="#64A2E1", fg='black', width = 12)
+
+name_entry = Entry(frame)
+email_entry = Entry(frame)
+pass_entry = Entry(frame,show="*")
+
+def display():
+ name = name_entry.get()
+ greet = "Hey"+name
+ message = "\nCongratulation for your new account!!"
+ textbox.insert(END,greet)
+ textbox.insert(END,message)
+
+textbox = Text(bg = "#B58D75", fg="black")
+
+btn = Button(text = "create account", command=display,bg="#831010")
+
+frame.place(x=20, y=0)
+lbl1.place(x=20, y=20)
+name_entry.place(x=150, y=20)
+lbl2.place(x=20, y=80)
+email_entry.place(x=150, y=80)
+lbl3.place(x=20, y=140)
+pass_entry.place(x=150, y=140)
+btn.place(x=130, y=210)
+textbox.place(y=250)
+
+root.mainloop()
