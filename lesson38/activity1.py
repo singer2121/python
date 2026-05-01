b9af741e139e59eb2c5c1d7a9f106acec048dc5c
singer2121 <singh.topperofschool@gmail.com>
2026-05-01 15:52:27 +0530

    inital commit

---
+++ lesson38/activity1.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+window = Tk()
+window.title("Event hadler")
+window.geometry("100x100")
+
+def handle_keypress(event):
+
+  """Print the character associated to the key pressed"""
+
+  print(event.char)
+
+window.bind("<Key>", handle_keypress)
+
+def handle_click(event):
+  print("\nThe Button was clicked!!!!!!")
+
+button = Button(text="Click me!")
+
+button.pack()
+button.bind("<Button-1>", handle_click)
+
+window.mainloop()
